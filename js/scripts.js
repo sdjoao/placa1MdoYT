6f84d1c16678ae097635a6cbc6badc91c0f2dbbf
sdjoao <sdjoaoasl@hotmail.com>
2024-10-22 19:46:15 -0300

    adicionei o javascript, que já estava previamente pronto do tutorial

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,25 @@ const _elements = {
 }
 
 _elements.channelNameDescription.addEventListener("input", () => {
-	
+	_elements.channelNameInput.value = _elements.channelNameDescription.innerText;
 });
 
 _elements.channelNameInput.addEventListener("input", (e) => {
-
+	_elements.channelNameDescription.innerText = _elements.channelNameInput.value;
 });
 
 _elements.button.addEventListener("click", () => {
-
+	if(_elements.button.classList.contains("button--broken"))
+			return;
+	inks.fx.shake(_elements.plaque, 15, false).then(brakePlaque);
 });
 
 const brakePlaque = () => {
-
+	_elements.button.classList.add("button--broken", "button--left-broken");
+	_elements.buttonPlay.classList.add("button__play--left-broken");
+	_elements.buttonRightBroken.classList.remove("button--hide");
 }
+
+_elements.channelNameInput.value = _elements.channelNameDescription.innerText;
+
+
